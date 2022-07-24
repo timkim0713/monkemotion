e8c02de0f688b18a4275210b671b9833b69286ea
Waan Choi <waan@SISs-MacBook-3.local>
2022-07-24 17:48:55 +0900

    update

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,43 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from "@mui/material/Typography"
+import Grid from "@mui/material/Grid"
+import { Button } from '@mui/material';
+
 function Footer() {
     return (
-        <Box sx={{ height: "50vh", backgroundColor: "lightGrey" }}>
-            {/* <p>Footer</p> */}
-            <Typography variant={"body1"}>Monkemotion Footer</Typography>
-        </Box>
+        <Grid
+        container
+        direction="column"
+        alignItems="center">
+            <Grid>
+                <Box sx={{ height: "50vh", backgroundColor: "lightGrey" }}>
+                {/* <p>Footer</p> */}
+                <Typography variant={"body1"}>Monkemotion Footer</Typography>
+                </Box>
+            </Grid>
+
+            <Grid
+            container
+            direction="row"
+            alignItems="center">
+                <Grid>
+                    <Button>HOME</Button>
+                </Grid>
+
+                <Grid>
+                    <Button>CONTRIBUTE</Button>
+                </Grid>
+
+                <Grid>
+                    <Button>RESEARCH</Button>
+                </Grid>
+
+                <Grid>
+                    <Button>ABOUT</Button>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 }
 
