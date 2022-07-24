32452043bd961afc030bb9536500c744765228e9
Daekyung Kim <timkim0713@gmail.com>
2022-07-24 17:38:47 +0900

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-
+import Typography from "@mui/material/Typography"
 function Footer() {
     return (
-        <Box sx={{height:"50vh",backgroundColor:"blue"}}>
-            <p>Footer</p>
+        <Box sx={{ height: "50vh", backgroundColor: "lightGrey" }}>
+            {/* <p>Footer</p> */}
+            <Typography variant={"body1"}>Monkemotion Footer</Typography>
         </Box>
     );
 }
