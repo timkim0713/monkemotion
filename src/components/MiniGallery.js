9cc7461a5afe75f9b1481270fee0a2fd2b227583
Daekyung Kim <timkim0713@gmail.com>
2022-07-29 20:05:30 +0900

    update

--- src/components/MiniGallery.js
+++ src/components/MiniGallery.js
@@ -5,41 +5,41 @@ import MultiCarousel from '../components/MultiCarousel';
 export default function MiniGallery() {
     return (
         <>
-<Box mt={5} sx={{
-                    display: "flex", flexDirection: "row"
-                }}>
+            <Box mt={5} sx={{
+                display: "flex", flexDirection: "row"
+            }}>
+                <Box sx={{
+                    background: "linear-gradient(to left, rgba(255,255,255,0), #fff)",
+                    height: 350,
+                    width: 100,
+                    position: "absolute",
+                    zIndex: 9999
+
+                }} />
+                <Box width={"100%"}>
+                    <MultiCarousel autoPlaySpeed={1200} rtl={false} />
+                </Box>
+
+                <Box>
                     <Box sx={{
-                        background: "linear-gradient(to left, rgba(255,255,255,0), #fff)",
+                        background: "linear-gradient(to right, rgba(255,255,255,0), #fff)",
                         height: 350,
                         width: 100,
                         position: "absolute",
-                        zIndex: 9999
+                        zIndex: 9999,
+                        marginLeft: -12
 
                     }} />
-                    <Box width={"100%"}>
-                        <MultiCarousel autoPlaySpeed={1200} rtl={false} />
-                    </Box>
-
-                    <Box>
-                        <Box sx={{
-                            background: "linear-gradient(to right, rgba(255,255,255,0), #fff)",
-                            height: 350,
-                            width: 100,
-                            position: "absolute",
-                            zIndex: 9999,
-                            marginLeft: -12
-
-                        }} />
-                    </Box>
-
                 </Box>
-                <Box mt={5}>
-                    <MultiCarousel autoPlaySpeed={1000} rtl={true} />
-                </Box>
-                <Box mt={5}>
-                    <MultiCarousel autoPlaySpeed={1100} rtl={false} />
-                </Box>
-                </>
-                   );
-                 }
+
+            </Box>
+            <Box mt={5}>
+                <MultiCarousel autoPlaySpeed={1000} rtl={true} />
+            </Box>
+            <Box mt={5}>
+                <MultiCarousel autoPlaySpeed={1100} rtl={false} />
+            </Box>
+        </>
+    );
+}
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,9 @@ import Box from '@mui/material/Box';
 import MainContent from '../MainContent';
 import MainCarousel from "../components/MainCarousel"
 import MiniGallery from '../components/MiniGallery';
+import { Button, Typography } from '@mui/material';
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
         <Box>
@@ -13,7 +16,29 @@ function Home() {
             </Box>
 
             <Box mt={10}>
+
                 <MiniGallery />
+
+                <Box display={"flex"} justifyContent={"center"} flexDirection={"column"} mt={6}>
+                    <Typography variant={"body2"} sx={{ textAlign: "center" }}>
+                        Contribute Now    Contribute Now    Contribute Now    Contribute Now
+                    </Typography>
+                    <Box display={"flex"} justifyContent={"center"} width={"100%"} mt={3}>
+
+
+                        <Link to={"/contribute"}>
+                            <Button variant={"outlined"} onClick={() => { }}>
+                                <Typography variant={"h6"}>
+                                    Contribute Now
+                                </Typography>
+                            </Button>
+                        </Link>
+
+
+                    </Box>
+                </Box>
+
+
             </Box>
 
         </Box >
