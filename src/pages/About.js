26703512e5e35007243ea6fafc2a4c5c02315ddb
Waan Choi <waan@SISs-MacBook-3.local>
2022-07-31 16:26:55 +0900

    hi

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,55 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import MainCard from '../components/MainCard';
+import Grid from '@mui/material/Grid';
+import { Card, CardContent, Typography } from '@mui/material';
 
 function About() {
     return (
-        <Box>
-            <p>About</p>
-        </Box>
+        <Box sx={{ width: "100%" }}>
+        <Grid container
+            direction={"row"}
+            justifyContent={"space-between"} columnSpacing={10}
+        >
+            <Grid item xs={12} md={6}>
+                <Card sx={{ height: 400, width: "100%", }}>
+                </Card>
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+                <Card sx={{ height: 400, width: "100%", }}>
+                    <CardContent>
+                        <Typography variant='h4'>monkemotion</Typography>
+                        <Typography variant='body1'>
+                            Description about monkemotion. Learn more!
+                        </Typography>
+
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
+        <Grid container
+            direction={"row"}
+            justifyContent={"space-between"} columnSpacing={10}
+        >
+            <Grid item xs={12} md={6}>
+                <Card sx={{ height: 400, width: "100%", }}>
+                </Card>
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+                <Card sx={{ height: 400, width: "100%", }}>
+                    <CardContent>
+                        <Typography variant='h4'>monkemotion</Typography>
+                        <Typography variant='body1'>
+                            Description about monkemotion. Learn more!
+                        </Typography>
+
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
+    </Box>
     );
 }
 
