6b6e433fadeda768ecaa2caab436ca08d9f88efb
Waan Choi <waan@SISs-MacBook-3.local>
2022-07-31 16:50:48 +0900

    d

--- src/pages/Contribute.js
+++ src/pages/Contribute.js
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import Heading from '../components/Heading';
+
 
 function Contribute() {
     return (
         <Box>
+            <Heading title={"Contribute"} content={"This is contribute page"} />
             <p>Contribute</p>
         </Box>
     );
