346a0e66d889d51907e3fe109f9da1ba242757fb
Daekyung Kim <timkim0713@gmail.com>
2022-07-31 17:55:09 +0900

    update

--- src/components/TagCard.js
+++ src/components/TagCard.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
@@ -8,25 +8,31 @@ import CardMedia from '@mui/material/CardMedia';
 import Grid from "@mui/material/Grid"
 import Box from "@mui/material/Box"
 function TagCard() {
+
+
+    const [currentImage, setCurrentImage] = useState("https://picsum.photos/400/300")
+    const [data, setData] = useState({})
+
+
+    function generateNewImage() {
+        const newImage = `https://picsum.photos/400/${Math.floor(Math.random() * 100 + 400)}`
+        setCurrentImage(newImage)
+    }
+
     return (
 
         <Grid item xs={12} md={7}>
             <Card sx={{ height: 425, width: "100%", }}>
-                {/* <CardMedia
-                    component="img"
-                    height="140"
-                    image="https://picsum.photos/seed/picsum/200/300"
-                    alt="green iguana"
-                /> */}
+
                 <CardContent sx={{ textAlign: "center" }}>
                     <Typography variant='body1' sx={{ m: 1 }}>Label</Typography>
-                    <img src="https://picsum.photos/seed/picsum/500/300" style={{ borderRadius: "10px" }}></img>
+                    <img src={currentImage} style={{ borderRadius: "10px" }}></img>
                 </CardContent>
 
             </Card>
             <Box display={"flex"} justifyContent={"flex-end"} sx={{ mr: -1, mt: 1 }}>
                 <CardActions>
-                    <Button variant='outlined' size="small">Generate New Image</Button>
+                    <Button variant='outlined' size="small" onClick={() => { generateNewImage() }}>Generate New Image</Button>
                     <Button variant='outlined' size="small">Submit Data</Button>
                 </CardActions>
             </Box>
