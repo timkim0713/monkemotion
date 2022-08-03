d8d495352d62e29bb75b0521bd9ec964973d430d
Daekyung Kim <timkim0713@gmail.com>
2022-08-03 20:40:33 +0900

    card img

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import MainCard from './components/MainCard';
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 import Box from "@mui/material/Box"
 
 function MainContent({ title, subtitle, content, img }) {
@@ -12,8 +12,13 @@ function MainContent({ title, subtitle, content, img }) {
                 justifyContent={"space-between"} columnSpacing={10}
             >
                 <Grid item xs={12} md={6}>
-                    <Card sx={{ height: 400, width: "100%", }}>
-                        <img src={img}></img>
+                    <Card>
+
+                        <CardMedia
+                            component="img"
+                            height="400"
+                            image={img}
+                        />
                     </Card>
                 </Grid>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ function Home() {
             <MainCarousel />
 
             <Box mt={10}>
-                <MainContent img={"https://mui.com/static/images/grid/complex.jpg"} title={"We Love Monkeys"} content={"Monkeys can help us."} subtitle={"August 3rd 2022"}/>
+                <MainContent img={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAATlBMVEXjHRn////hAAD0vLz87ezyrKv75+f3zczjFxLiEgzlPTvpYV/sgYDjGRXiDwn40dHlNTP1wsL99PTrdnTxpqXnUU/lOjfqaGbnS0npX12OVvhKAAABzElEQVR4nO3cy27iQBBAUfCQweYRXgkJ//+js7TLEaUMlBFC52ybTulm5U7azGYAAAAAAAAAAAAAAAAAAAAAvKImM83ILhv5Xj2tOfy57m2SxG6dzDxsqhObeeI4SWGzyWaW/1bTwoXC20YqLB6nsJ7C6nEK6ymsHqewnsLqcYu/Q8cHFL6f4syJC0fnw48HFM7e48zFxIVB8/aIwtFMhbXTFE4xU2HtNIVTzFRYO03hFDMV1k5TOLCNz89tWGzj4jab+ayF2/1uObD7HCa2n3FxnyQ+bWG7Ch+dr7vBYreOi6v26s9RWEzhgEKF/UyFpRQOKFTYz1RY6o7CS9P1mssLFI7PFqfLunc5vcDZYnw+3IWduxc4H453LsPO5X/sVFhKYbJT4fWdCkspTHYqvL5TYSmFyc7nLczvRCWvs9z+5J3eifqIiwWB2b22eVhbnIaJ23PYufg6r3rnr7h4Hibm99qOceb9iendxJHNcNzPE3DbS0/A+d3EkScq/P1fMRQqVKhQoUKFChUq/FmYvY8/elU+RGz3YefhO9z6+o6L4T8z6fv445kVx6ff68LG22/upd+pMHJ/IAAAAAAAAAAAAAAAAAAAAABP6B/BYC8m2V+4agAAAABJRU5ErkJggg=="} title={"We Love Monkeys"} content={"Monkeys can help us."} subtitle={"August 3rd 2022"} />
             </Box>
 
             <Box mt={10}>
