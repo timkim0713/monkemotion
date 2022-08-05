50c515d33efa1694a6a324643d357d889bfb751c
Waan Choi <waan@SISs-MacBook-3.local>
2022-08-05 20:02:24 +0900

    D

--- src/components/MainCarousel.js
+++ src/components/MainCarousel.js
@@ -10,12 +10,12 @@ export default function MainCarousel() {
                 <img
                     height={400}
                     style={{ objectFit: "cover" }}
-                    src="https://images.newscientist.com/wp-content/uploads/2022/02/01174019/PRI_221086136.jpg?crop=16:9,smart&width=1200&height=675&upscale=true" />
+                    src="https://www.awf.org/sites/default/files/SpeciesPage_AfricanWildDog01_Hero_0.jpg" />
                 <p className="legend" style={{ backgroundColor: "#613f16" }}>Olive Baboons (Papio Anubis) that are located at the Seneca Park Zoo are used for this study.</p>
             </div>
             <div>
                 <img style={{ objectFit: "cover" }}
-                    height={400} src="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_1200,h_900/https://neprimateconservancy.org/wp-content/uploads/2021/10/olive-baboon-animal-1307280-en-resize.jpg" />
+                    height={400} src="https://www.mpalalive.org/uploads/gallery/olive-baboon_gallery_4.jpg" />
                 <p className="legend" style={{ backgroundColor: "#613f16" }}>Olive Baboons live in groups, also known as "troops", in the wild.</p>
             </div>
             <div>
