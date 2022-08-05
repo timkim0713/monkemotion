11239a0ae6577b940928360dd8324deb66d97b98
Waan Choi <waan@SISs-MacBook-3.local>
2022-08-05 19:34:17 +0900

    d

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,11 +13,11 @@ function About() {
         <Box sx={{ width: "100%" }}>
             <Heading title={"About"} content={"Learn about what we do."} />
             <Box mt={10}>
-                <MainContent img={"https://mui.com/static/images/grid/complex.jpg"} title={"Abc"} content={"content"} subtitle={"sub1"} />
+                <MainContent img={"https://cdn.freelogodesign.org/files/71166f7ec8f34d319d8611605c3f01d3/thumb/logo_200x200.png?v=0"} title={"What is MONKEMOTION?"} content={"MONKEMOTION is an interactive tool which allows users to participate in the collection of monkey and primate images. "} subtitle={"MONKEMOTION is a combination of the words 'monkey' and 'emotion'."} />
             </Box>
 
             <Box mt={10}>
-                <MainContent img={"https://mui.com/static/images/grid/complex.jpg"} title={"Abc"} content={"content"} subtitle={"sub2"} />
+                <MainContent img={"https://media.istockphoto.com/photos/male-chimpanzee-in-business-clothes-picture-id184941527?k=20&m=184941527&s=612x612&w=0&h=ETZiNiSQG18Bn5oUI7Wzcj4Tk-4hCTtWTQBEDXEzvCs="} title={"Have any questions?"} content={"Any questions or concerns may be submitted through the link here."} subtitle={"Feel free to ask any questions."} />
             </Box>
 
         </Box>
