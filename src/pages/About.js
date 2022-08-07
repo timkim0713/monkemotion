9c103b0bd9f65f40cf3901049eb84e1497858d0b
Waan Choi <waan@SISs-MacBook-3.local>
2022-08-07 16:46:46 +0900

    d

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,7 +13,7 @@ function About() {
         <Box sx={{ width: "100%" }}>
             <Heading title={"About"} content={"Learn about what we do."} />
             <Box mt={10}>
-                <MainContent img={"https://cdn.freelogodesign.org/files/71166f7ec8f34d319d8611605c3f01d3/thumb/logo_200x200.png?v=0"} title={"What is MONKEMOTION?"} content={"MONKEMOTION is an interactive tool which allows users to participate in the collection of monkey and primate images. "} subtitle={"MONKEMOTION is a combination of the words 'monkey' and 'emotion'."} />
+                <MainContent img={"https://cdn.freelogodesign.org/files/71166f7ec8f34d319d8611605c3f01d3/thumb/logo_200x200.png?v=0"} title={"What is MONKEMOTION?"} content={"MONKEMOTION is an interactive tool that allows anyone to participate in the collection of monkey and primate images. By locating the eyes, noses, mouths, and ears of monkeys, users can directly contribute to creating a big data base of facial structures of monkeys. The data base is used by an AI that would be used to identify the emotional states of monkeys simply by scanning their faces. It is currently being used in a study - conducted by Carnegie Mellon University - related to olive baboons located at the Seneca Park Zoo."} subtitle={"MONKEMOTION is a combination of the words 'monkey' and 'emotion'."} />
             </Box>
 
             <Box mt={10}>
